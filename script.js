42bf9cab32ecee1c3468015ec7fa98d7b885cd03
achahid19 <anasks1999@gmail.com>
2025-04-29 16:32:04 +0100

    first init

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+// Using browser geolocation API
+navigator.geolocation.getCurrentPosition(function(position) {
+		const {latitude} = position.coords;
+		const {longitude} = position.coords;
+
+		//console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+		const coords = [latitude, longitude];
+		const map = L.map('map').setView(coords, 13);
+
+		L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+		}).addTo(map);
+
+		// leaflit map eventListenner
+		map.on('click', function(mapEvent) {
+			const {lat, lng} = mapEvent.latlng;
+
+			L.marker([lat, lng]).addTo(map).bindPopup(
+			L.popup({
+				maxWidth: 250,
+				minWidth: 100,
+				autoClose: false,
+				closeOnClick: false,
+				className: 'running-popup',
+			})).setPopupContent('Running').openPopup();
+		})
+	}, function() {
+		alert('Cannot fetch current user position!');
+	}
+)
